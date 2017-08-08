4c5ad680fd77638af8d7b16837ec80e6652f07b7
Javier Ruiz Aranguren <jruizaranguren@gmail.com>
2017-08-08 17:54:11 +0200

    try to mount correctly to input folder

--- face_generation/runfloyd.sh
+++ face_generation/runfloyd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Starting floyd job..."
-floyd run --mode jupyter --gpu --env tensorflow 1.2 --data R5KrjnANiKVhLWAkpXhNBe
+floyd run --mode jupyter --gpu --env tensorflow 1.2 --data R5KrjnANiKVhLWAkpXhNBe:input
 echo "Job Floyd started"
